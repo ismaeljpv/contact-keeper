df9d5220bb4d7f87ac7e3eb4f4422849b582b2d6
Ismael Pena <ismael.pena@bairesdev.com>
2021-09-14 16:13:30 -0400

    adding client mock integration

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this page</h1>
+            <p className="lead my-1">This is a fullstack React App for keeping contacts.</p>
+            <p className="bg-dark p"><strong>V-1.0.0</strong></p>
+        </div>
+    )
+};
+
+export default About;
